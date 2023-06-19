d9b6b66cc9d6747fd9b143eb581f97295b8bf7e2
sat2llite <raumschiff_@naver.com>
2023-06-19 10:40:55 +0900

    마무리작업중

--- js/result.js
+++ js/result.js
@@ -2,7 +2,8 @@
 const resultTitle = document.querySelector(".result_wrap > h3"); // 점수(결과)
 const resultText = document.querySelector(".result_wrap > p"); // 틀린 문제
 const replayBtn = document.querySelector(".replay_btn"); // 다시하기 버튼
-const tableResult = document.querySelectorAll(".table_result > td");
+const tableResult = document.querySelectorAll(".table_result > td > img");
+// const createImage = document.createElement("img");
 
 // locaStorage
 const nameString = window.localStorage.getItem("name");
@@ -13,9 +14,12 @@ console.log(scoreString);
 resultTitle.innerHTML = `${nameString}님의 점수는 ${resultString}점 입니다.`;
 
 tableResult.forEach((item, index) => {
+  
   item.innerHTML = `${scoreString[index]}`;
   if (scoreString[index]) {
-    tableResult.src = "img/o.png";
+    item.src = "img/o.png";
+  } else {
+    item.src="img/x.png"
   }
 })
 
